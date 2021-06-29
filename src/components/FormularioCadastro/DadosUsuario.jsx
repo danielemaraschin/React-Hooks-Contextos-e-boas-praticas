622526c9c09a347cdb173e98023d57b1cf81f181
danielemaraschin <danikmaraschin@gmail.com>
2021-06-30 01:16:16 +0200

    Creating new components

--- src/components/FormularioCadastro/DadosUsuario.jsx
+++ src/components/FormularioCadastro/DadosUsuario.jsx
@@ -5,6 +5,8 @@ function DadosUsuario(){
     return(
         <form>
             <TextField id="email" label="email" type="email"/>
+            <TextField id="senha" label="senha" type="password"/>
+            <Button type="submit"> Cadastrar</Button>
         </form>
     )
 }
