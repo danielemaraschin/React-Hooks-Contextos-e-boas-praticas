8ba0b2b14fa0ea212414e79b6e96d392dbf66bd6
danielemaraschin <danikmaraschin@gmail.com>
2021-06-30 01:04:57 +0200

    Create new form and importing

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
 import { TextField, Button, Switch, FormControlLabel } from "@material-ui/core";
 import DadosPessoais from "./DadosPessoais";
+import DadosUsuario from "./DadosUsuario";
 
 function FormularioCadastro({aoEnviar, validarCPF}) {
     return (
+      <>
    <DadosPessoais/>
+   <DadosUsuario/>
+   |</>
   );
 }
 
