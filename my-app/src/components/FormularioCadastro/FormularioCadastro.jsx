cbf637c1016f153792c78fc41ebf106cf4f9a51c
danielemaraschin <danikmaraschin@gmail.com>
2021-06-30 18:16:22 +0200

    working with switch case

--- my-app/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ my-app/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,17 +1,30 @@
 import React, { useState } from "react";
-import { TextField, Button, Switch, FormControlLabel } from "@material-ui/core";
+import { Typography } from "@material-ui/core";
 import DadosPessoais from "./DadosPessoais";
 import DadosUsuario from "./DadosUsuario";
 import DadosEntrega from "./DadosEntrega";
 
 function FormularioCadastro({ aoEnviar, validarCPF }) {
+  const [etapaAtual, setEtapaAtual] = useState(1);
+
+  function formularioAtual(etapa) {
+    switch (etapa) {
+      case 0:
+        return <DadosUsuario />
+      case 1:
+        return <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF} />
+      case 2:
+        return <DadosEntrega />
+      default:
+        return <Typography>Erro ao selecionar formulário</Typography>
+    }
+  }
   return (
     <>                  {/* colocar as propriedades ao enviar e validar CPF do elemento Dados pessoais*/}
-      <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF}/>
-      <DadosUsuario />
-      <DadosEntrega/>
+      {formularioAtual(etapaAtual)} {/*função q indica qual formulario renderizar*/}
     </>
   );
 }
+
 
 export default FormularioCadastro;
