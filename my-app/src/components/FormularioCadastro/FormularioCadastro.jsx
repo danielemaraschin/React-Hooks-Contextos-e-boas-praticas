68f5e366fea1f00ad4c723eaa97767434a675fcf
danielemaraschin <danikmaraschin@gmail.com>
2021-06-30 17:04:32 +0200

    Creating new components

--- my-app/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ my-app/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -2,13 +2,15 @@ import React, { useState } from "react";
 import { TextField, Button, Switch, FormControlLabel } from "@material-ui/core";
 import DadosPessoais from "./DadosPessoais";
 import DadosUsuario from "./DadosUsuario";
+import DadosEntrega from "./DadosEntrega";
 
 function FormularioCadastro({ aoEnviar, validarCPF }) {
   return (
     <>                  {/* colocar as propriedades ao enviar e validar CPF do elemento Dados pessoais*/}
       <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF}/>
       <DadosUsuario />
-      |</>
+      <DadosEntrega/>
+    </>
   );
 }
 
