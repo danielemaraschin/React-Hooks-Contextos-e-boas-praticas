7b844b3b647e66a9b93b85321771b8d22669f1c5
danielemaraschin <danikmaraschin@gmail.com>
2021-07-01 10:03:01 +0200

    working with hooks spread operator

--- my-app/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ my-app/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -5,13 +5,21 @@ import DadosEntrega from "./DadosEntrega";
 
 function FormularioCadastro({ aoEnviar, validarCPF }) {
   const [etapaAtual, setEtapaAtual] = useState(0);
+  const [dadosColetados, setDados] = useState({});//usestate recebe objeto vazio
+
 
   const formularios = [
-    <DadosUsuario aoEnviar={proximo} />,
-    <DadosPessoais aoEnviar={proximo} validarCPF={validarCPF} />,
-    <DadosEntrega aoEnviar={aoEnviar} />
+    <DadosUsuario aoEnviar={coletarDados} />,
+    <DadosPessoais aoEnviar={coletarDados} validarCPF={validarCPF} />,
+    <DadosEntrega aoEnviar={coletarDados} />
   ];
 
+  function coletarDados(dados){
+    setDados({...dadosColetados, ...dados}) //spread opeator para nao jogar
+    // o objeto inteiro e sim cada atributo e jogar no objeto que está criando
+    console.log(dadosColetados);
+    proximo();
+  }
   function proximo() {
     setEtapaAtual(etapaAtual + 1);
   }
