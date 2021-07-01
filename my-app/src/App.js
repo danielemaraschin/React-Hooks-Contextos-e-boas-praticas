51ea0cd5c74f5cfca94f338f63f97e69a805fd24
danielemaraschin <danikmaraschin@gmail.com>
2021-07-01 17:06:05 +0200

    working with Validations

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,15 +2,16 @@ import React, { Component } from "react";
 import "./App.css";
 import FormularioCadastro from "./components/FormularioCadastro/FormularioCadastro";
 import 'fontsource-roboto';
+import { validarCPF, validarSenha } from "./models/cadastro";
 
 import {Container, Typography } from "@material-ui/core"
 class App extends Component {
   render() {
     return (
       <Container component="article" maxWidth="sm">
-        <Typography variant="h3" component="h1" align="center" >Formulário de cadastro</Typography>
-        <FormularioCadastro aoEnviar={aoEnviarForm} validarCPF={validarCPF} />
-      </Container>
+        <Typography variant="h3" component="h1" align="center" >Formulário de cadastro</Typography>      {/*1{} CODE JS*/}
+        <FormularioCadastro aoEnviar={aoEnviarForm} validacoes={{cpf:validarCPF, senha:validarSenha}}/> {/*2 {{}} OBJECT JS*/}
+      </Container>                                                                                     
     );
   }
 }
